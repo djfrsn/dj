fc5a721dcd88d522741055497e7936b518ed590b
Dennis Jefferson <dj@dennisjefferson.com>
2014-11-02 21:10:21 -0800

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,12 @@
+
+// Set height for vertically centered tables
+function setTableHeight() {
+var winHeight = $(window).height();
+$('.table').css('height', winHeight);
+$('.table__cell').css('height', winHeight);
+}setTableHeight();
+
+// Reset table height on window resize
+$(window).resize(function() {
+ setTableHeight();
+});
