9bd4272553001544f567ee6030dc9d402883f91b
Dennis Jefferson <dj@dennisjefferson.com>
2014-11-02 22:20:35 -0800

    NavBar: off canvas menu

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,36 @@ $('.table__cell').css('height', winHeight);
 $(window).resize(function() {
  setTableHeight();
 });
+
+
+// Call functions on document.ready
+$(document).ready(function(){												
+ 
+       //Navigation Menu Slider
+        $('#nav-expander').on('click',function(e){
+      		e.preventDefault();
+      		$('body').toggleClass('nav-expanded');
+      	});
+      	$('#nav-close').on('click',function(e){
+      		e.preventDefault();
+      		$('body').removeClass('nav-expanded');
+      	});
+ 
+      	// Initialize navgoco with default options
+        $(".main-menu").navgoco({
+            caret: '<span class="caret"></span>',
+            accordion: false,
+            openClass: 'open',
+            save: true,
+            cookie: {
+                name: 'navgoco',
+                expires: false,
+                path: '/'
+            },
+            slide: {
+                duration: 300,
+                easing: 'swing'
+            }
+        });
+ 
+      });
